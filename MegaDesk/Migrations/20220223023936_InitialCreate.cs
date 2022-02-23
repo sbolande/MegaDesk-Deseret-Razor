a8df9416a3db2eef85ee6986cf4905de22945ea3
Seth Bolander <bol15015@byui.edu>
2022-02-22 19:40:24 -0700

    Created database

---
+++ MegaDesk/Migrations/20220223023936_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MegaDesk.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DeskQuote",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Width = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Depth = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DrawerCount = table.Column<int>(type: "int", nullable: false),
+                    DesktopMaterial = table.Column<int>(type: "int", nullable: false),
+                    ProductionDays = table.Column<int>(type: "int", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    QuoteDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Value = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeskQuote", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DeskQuote");
+        }
+    }
+}
