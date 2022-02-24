f7406a428529893cb550588a5517c48d0fa9b1d9
archiblue22 <paguirre@byui.edu>
2022-02-24 12:25:30 -0700

    Completed: search quotes by customer name & sort quotes by date/name only

--- MegaDesk/Pages/DeskQuotes/Index.cshtml.cs
+++ MegaDesk/Pages/DeskQuotes/Index.cshtml.cs
@@ -20,11 +20,62 @@ namespace MegaDesk.Pages.DeskQuotes
             _context = context;
         }
 
+        // Sorting Functionality
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
+
         public IList<DeskQuote> DeskQuote { get;set; }
 
-        public async Task OnGetAsync()
+        // Search Functionality
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchSelection { get; set; }
+
+        public async Task OnGetAsync(string sortOrder)
         {
-            DeskQuote = await _context.DeskQuote.ToListAsync();
+            var quotes = from m in _context.DeskQuote
+                         select m;
+
+            // Sorting
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    quotes = quotes.OrderByDescending(s => s.FirstName);
+                    break;
+                case "Date":
+                    quotes = quotes.OrderBy(s => s.QuoteDate);
+                    break;
+                case "date_desc":
+                    quotes = quotes.OrderByDescending(s => s.QuoteDate);
+                    break;
+                default:
+                    quotes = quotes.OrderBy(s => s.FirstName);
+                    break;
+            }
+
+            // Searching
+            if (SearchSelection == "FirstName")
+            {
+                if (!string.IsNullOrEmpty(SearchString))
+                {
+                    quotes = quotes.Where(s => s.FirstName.Contains(SearchString));
+                }
+            }
+            else if (SearchSelection == "LastName")
+            {
+                if (!string.IsNullOrEmpty(SearchSelection))
+                {
+                    quotes = quotes.Where(s => s.LastName.Contains(SearchString));
+                }
+            }
+
+            DeskQuote = await quotes.ToListAsync();
         }
     }
 }
